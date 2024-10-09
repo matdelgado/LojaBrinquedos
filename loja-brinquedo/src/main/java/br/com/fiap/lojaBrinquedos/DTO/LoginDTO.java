8fc77fd4c8627c1185d00f2ba05fcf3e6cb4c8ae
DennerDuarte <rm551938@fiap.com.br>
2024-10-09 20:36:22 -0300

    Classe de Login DTO criada

--- loja-brinquedo/src/main/java/br/com/fiap/lojaBrinquedos/DTO/LoginDTO.java
+++ loja-brinquedo/src/main/java/br/com/fiap/lojaBrinquedos/DTO/LoginDTO.java
@@ -1,4 +1,48 @@
 package br.com.fiap.lojaBrinquedos.DTO;
 
+import br.com.fiap.lojaBrinquedos.Controller.BrinquedoController;
+import br.com.fiap.lojaBrinquedos.Controller.LoginController;
+import lombok.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginDTO {
+
+    public Long id;
+    public String user;
+    public String senha;
+
+    public static EntityModel<LoginDTO> toModel(LoginDTO login) {
+        Link selfLink = WebMvcLinkBuilder.linkTo(BrinquedoController.class)
+                .slash(login.getId())
+                .withSelfRel();
+
+        Link getAllLink = WebMvcLinkBuilder.linkTo(LoginController.class)
+                .withRel("getAll");
+
+        Link getByIdLink = WebMvcLinkBuilder.linkTo(LoginController.class)
+                .slash(login.getId())
+                .withRel("getById");
+
+        Link createLink = WebMvcLinkBuilder.linkTo(LoginController.class)
+                .withRel("create");
+
+        Link updateLink = WebMvcLinkBuilder.linkTo(LoginController.class)
+                .slash(login.getId())
+                .withRel("update");
+
+        Link deleteLink = WebMvcLinkBuilder.linkTo(LoginController.class)
+                .slash(login.getId())
+                .withRel("delete");
+
+        return EntityModel.of(login, selfLink, getAllLink, getByIdLink, createLink, updateLink, deleteLink);
+    }
+
 }
